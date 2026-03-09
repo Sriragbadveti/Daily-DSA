d5ced536dd5b8d8b37f8bda142388bef6fe5c689
Sriragbadveti <84879813+Sriragbadveti@users.noreply.github.com>
2026-03-09 22:44:54 +0400

    Time: 0 ms (100%), Space: 53.1 MB (17.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -10,14 +10,14 @@ class Solution {
 
 
             int mid = (left+right)/2;
-            if(arr[mid] == arr[mid+1]){
+            if(mid<n-1 && arr[mid] == arr[mid+1]){
                 int isOdd = mid-left;
                 if(isOdd%2==0){
                     left = mid+2;
                 }else{
                     right = mid-1;
                 }
-            }else if(arr[mid] == arr[mid-1]){
+            }else if(mid>0 && arr[mid] == arr[mid-1]){
                 int isOdd = mid-1-left;
                 if(isOdd%2==0){
                     left = mid+1;
