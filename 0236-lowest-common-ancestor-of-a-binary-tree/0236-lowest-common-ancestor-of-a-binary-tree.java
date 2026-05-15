ee8fc5e500c26bad85058ce021bf4441d4990572
Sriragbadveti <84879813+Sriragbadveti@users.noreply.github.com>
2026-05-15 19:20:53 +0400

    Time: 7 ms (98.84%), Space: 46.5 MB (68.44%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,19 +9,19 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        // we are starting a pre order traversal because the root itself can also be the answer so lets process the root first and then the left and right subtree
 
-        if (root == null)
-            return null;
+        
+        if(root == null) return null;
+        if(root== p || root==q) return root;
 
-        if (root == p || root == q)
-            return root;
+        //explore left and right subtree
+        TreeNode left = lowestCommonAncestor(root.left , p , q);
+        TreeNode right = lowestCommonAncestor(root.right , p , q);
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        if (left != null && right != null)
-            return root;
-
-        return left == null ? right : left;
+        //now if left reaches null then automaticall right becomes the answer else left is the one 
+        if(left!=null && right!=null) return root;
+        return left==null ? right : left;
     }
 }
