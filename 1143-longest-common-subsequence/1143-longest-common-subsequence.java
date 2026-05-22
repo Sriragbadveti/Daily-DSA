93d52d5dfd9aee9c3b8fefdca4f0801a9d440cae
Sriragbadveti <84879813+Sriragbadveti@users.noreply.github.com>
2026-05-23 00:44:15 +0400

    Time: 19 ms (91.08%), Space: 53.9 MB (50.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,34 +1,30 @@
 class Solution {
-    int dp[][] ;
     public int longestCommonSubsequence(String text1, String text2) {
-        
-        dp = new int[text1.length()][text2.length()];
-         for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        // this the tabulation (bottom - up) method for this problem
+
+        int n = text1.length();
+        int m = text2.length();
+
+        int dp[][] = new int [n+1][m+1];
+
+        for(int i = n-1 ;i>=0 ; i--){
+            for(int j = m-1; j>=0; j--){
+
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i][j] =
+                        1 + dp[i + 1][j + 1];
+                }else{
+
+                    dp[i][j] =  Math.max(
+                            dp[i + 1][j],
+                            dp[i][j + 1]
+                        );
+                }
+
+
+            }
         }
-        return traversal(text1 , text2 , 0 , 0 );
-    }
 
-    public  int traversal(String text1 , String text2 , int n , int m){
-
-        //base case
-        if(n==text1.length() || m==text2.length())return 0;
-
-        //check cache
-        if(dp[n][m] != -1){
-            return dp[n][m];
-        }
-
-        //choices
-
-        
-        if(text1.charAt(n) == text2.charAt(m)){
-            dp[n][m]  = 1+traversal(text1 , text2 , n+1 , m+1);
-            return dp[n][m];
-        }else{
-
-            dp[n][m] = (Math.max(traversal(text1 , text2 , n+1 , m) , traversal(text1 , text2 , n , m+1)));
-            return dp[n][m];
-        }
+        return dp[0][0];
     }
 }
