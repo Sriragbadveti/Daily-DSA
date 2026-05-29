0b97b1d9e5fd7193f7c160fe1dfbf53a774448bf
Sriragbadveti <84879813+Sriragbadveti@users.noreply.github.com>
2026-05-29 22:47:23 +0400

    Time: 10 ms (12.74%), Space: 44.4 MB (82.84%) - LeetHub

---
+++ 3853-merge-close-characters/3853-merge-close-characters.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String mergeCharacters(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+       
+
+        while(true) {
+
+
+            boolean merged = false;
+            //find the smallest left index
+            for(int i = 0 ; i<sb.length() ; i++){
+
+                //find the smallest right index for the left index
+                for(int j = i+1 ; j<sb.length() ;j++){
+
+                    if(sb.charAt(i) == sb.charAt(j) && j-i<=k){
+                        sb.deleteCharAt(j);
+                        merged = true;
+                        break;
+                    }
+
+                }
+
+                if(merged) {
+                    break;
+                }
+            }
+
+            if(!merged)break;
+
+            
+        }
+        return sb.toString();
+    }
+}
