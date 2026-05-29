e585fc2809bd90a1a11d53ba782350bc0d98becf
Sriragbadveti <84879813+Sriragbadveti@users.noreply.github.com>
2026-05-29 04:12:19 +0400

    Time: 84 ms (21.62%), Space: 237.2 MB (9.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,34 +1,24 @@
 class Solution {
-    int dp[][];
     public int lengthOfLIS(int[] nums) {
+        //this is the tabulation solution
         int n = nums.length;
-        dp = new int[n][n];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[n+1][n+1];
+
+        //we have to start filling from the backward because recurrence depends on index+1
+
+        for(int index = n-1 ; index>=0 ; index--){
+            for(int prev = index-1 ; prev>=-1 ; prev--){
+
+                int skip = dp[index+1][prev+1];
+
+                int take  = 0 ;
+                if( prev==-1  || nums[index] > nums[prev] ){
+                    take = 1+dp[index+1][index+1];
+                }
+                dp[index][prev+1] = Math.max(skip , take);
+            }
         }
+        return dp[0][0];
 
-        return traversal(nums , 0 , -1);
-    }
-
-    public int traversal(int nums[] , int index , int prev){
-
-        int n = nums.length;
-        //base case
-
-        if(index==n)return 0;
-
-        //check cache
-        if(dp[index][prev+1] != -1)return dp[index][prev+1];
-
-        int skip = traversal(nums , index+1 , prev);
-
-        int take = 0;
-        if(prev == -1 || nums[index] > nums[prev]){
-            take = 1+traversal(nums , index+1 , index);         
-        }    
-
-        return dp[index][prev+1] = Math.max(take , skip);
-        
-        
     }
 }
